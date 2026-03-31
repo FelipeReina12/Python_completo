a295ad71e09ca54812d88725574271db2f0138a0
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2026-03-31 09:45:07 -0500

    Continuando

---
+++ Modulo-1-Fundamentos-y-Lógica-de-Programación/02-Estructuras-de-Control-y-Colecciones/Ciclo-for.py
@@ -0,0 +1,3 @@
+# El ciclo for es una estructura de contról que nos permite repetir un bloque de código un número 
+# determinado de veces. Es especialmente útil cuando queremos iterar sobre una secuencia de elementos, como 
+# una lista, una tupla, un diccionario o un rango de números.
---
+++ Modulo-1-Fundamentos-y-Lógica-de-Programación/02-Estructuras-de-Control-y-Colecciones/Metodos-de-listas.py
@@ -0,0 +1,17 @@
+# Crear una lista con list()
+lista = list(["Felipe", "Clarinete", "Mackbook", "Nintendo"])
+
+# Devolver la cantidad de elementos de una lista
+numero_elementos = len(lista)
+
+# Agregar elementos a las listas en la última poscición
+# Con append
+nueva_lista = lista.append("Xbox")
+
+
+
+
+
+
+print(nueva_lista)
+
